86039442b51fb3036a35e7a4938630a8392a9ea3
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2014-01-28 23:19:53 +0000

    Get contents of json dir; create if needed

--- fetch_metadata.py
+++ fetch_metadata.py
@@ -5,6 +5,7 @@ DATA_PATH = 'data'
 URI_DATA_PATH = os.path.join(DATA_PATH, 'uris')
 JSON_DATA_PATH = os.path.join(DATA_PATH, 'json')
 
+
 def get_uri_file_names(path=URI_DATA_PATH):
     try:
         result = os.listdir(path)
@@ -20,8 +21,22 @@ def get_uri_file_names(path=URI_DATA_PATH):
     ))
     return result
 
+def get_json_file_names(path=JSON_DATA_PATH):
+    try:
+        result = os.listdir(path)
+    except OSError:
+        os.mkdir(path)
+        sys.stdout.write("json dir created\n")
+        result = []
+    sys.stdout.write("json dir contains {count} files:\n    {files}\n".format(
+        count=len(result),
+        files=", ".join(result)
+    ))
+    return result
+
 def main():
     uri_file_names = get_uri_file_names()
+    json_file_names = get_json_file_names()
 
 
 if __name__ == "__main__":
